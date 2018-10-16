7921b409adacdba5dff4accc6070e2ad45c4d7f7
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-10-16 20:09:45 +0200

    :lipstick: Update Button component

---
+++ src/components/Button/stories.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text, select } from '@storybook/addon-knobs'
+import Button from '.'
+
+const stories = storiesOf('Components/Button', module)
+
+export const Default = () => (
+  <Button
+    color={select(
+      'color',
+      {
+        default: 'default',
+        primary: 'primary',
+        secondary: 'secondary',
+      },
+      'default',
+    )}
+    size={select(
+      'size',
+      {
+        small: 'small',
+        medium: 'medium',
+        large: 'large',
+      },
+      'medium',
+    )}
+  >
+    {text('children', 'Just a button')}
+  </Button>
+)
+
+stories.add('Default', Default)
+
+export default Default
