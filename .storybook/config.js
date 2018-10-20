8c623740daa901251813dd30de50e313a638e792
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-10-20 19:47:19 +0200

    :zap: Base styling for Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { addDecorator, configure } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options'
 import { withKnobs } from '@storybook/addon-knobs'
-import '../src/components/styles/main.module.scss'
+
+import '../src/components/styles/base.module.scss'
 
 const components = require.context('../src/components', true, /\/stories\.js$/)
 
