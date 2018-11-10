2689e5c2141d1bdabff51d97d46b55669f044d42
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-11-10 18:56:30 +0100

    :wrench: Change config to read page stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,10 +6,13 @@ import '../src/components/styles/base.module.scss'
 
 const components = require.context('../src/components', true, /\/stories\.js$/)
 const containers = require.context('../src/containers', true, /\/stories\.js$/)
+const pages = require.context('../src/pages', true, /stories\.js$/)
+
 
 function loadStories() {
 	components.keys().forEach(filename => components(filename))
 	containers.keys().forEach(filename => containers(filename))
+	pages.keys().forEach(filename => pages(filename))
 }
 
 setOptions({
