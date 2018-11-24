a52a517d35f5fbadd79e2281017823ae5ed6494d
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-11-24 22:02:40 +0100

    :art: Refactoring

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -30,23 +30,25 @@ const Grid = props => {
 			[styles.container]: container,
 			[styles.item]: item,
 			[styles.zeroMinWidth]: zeroMinWidth,
-			[styles[`spacing-xs-${String(spacing)}`]]:
-				container && spacing !== 0,
+			[styles[`spacing-${String(spacing)}`]]: container && spacing !== 0,
 			[styles[`direction-xs-${String(direction)}`]]:
 				direction !== Grid.defaultProps.direction,
-			[styles[`wrap-xs-${String(wrap)}`]]:
-				wrap !== Grid.defaultProps.wrap,
+			[styles[`wrap-xs-${String(wrap)}`]]: wrap !== Grid.defaultProps.wrap,
 			[styles[`align-items-xs-${String(alignItems)}`]]:
 				alignItems !== Grid.defaultProps.alignItems,
 			[styles[`align-content-xs-${String(alignContent)}`]]:
 				alignContent !== Grid.defaultProps.alignContent,
-			[styles[`justify-xs-${String(justify)}`]]:
-				justify !== Grid.defaultProps.justify,
-			[styles[`grid-xs-${String(xs)}`]]: xs !== false,
-			[styles[`grid-sm-${String(sm)}`]]: sm !== false,
-			[styles[`grid-md-${String(md)}`]]: md !== false,
-			[styles[`grid-lg-${String(lg)}`]]: lg !== false,
-			[styles[`grid-xl-${String(xl)}`]]: xl !== false,
+			[styles[`justify-xs-${String(justify)}`]]: justify !== Grid.defaultProps.justify,
+			[styles['grid-xs']]: xs === true,
+			[styles[`grid-xs-${String(xs)}`]]: xs && xs !== true,
+			[styles['grid-sm']]: sm === true,
+			[styles[`grid-sm-${String(sm)}`]]: sm && sm !== true,
+			[styles['grid-md']]: md === true,
+			[styles[`grid-md-${String(md)}`]]: md && md !== true,
+			[styles['grid-lg']]: lg === true,
+			[styles[`grid-lg-${String(lg)}`]]: lg && lg !== true,
+			[styles['grid-xl']]: xl === true,
+			[styles[`grid-xl-${String(xl)}`]]: xl && xl !== true,
 		},
 		classNameProp,
 	)
@@ -54,8 +56,18 @@ const Grid = props => {
 }
 
 Grid.propTypes = {
-	children: PropTypes.node.isRequired,
+	/**
+	 * The component used for the root node.
+	 * Either a string to use a DOM element or a component.
+	 */
+	component: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+	/**
+	 * The CSS class name of the root element.
+	 */
 	className: PropTypes.string,
+	/**
+	 * Defines the `align-content` style property.
+	 */
 	alignContent: PropTypes.oneOf([
 		'stretch',
 		'center',
@@ -64,13 +76,13 @@ Grid.propTypes = {
 		'space-between',
 		'space-around',
 	]),
-	alignItems: PropTypes.oneOf([
-		'flex-start',
-		'center',
-		'flex-end',
-		'stretch',
-		'baseline',
-	]),
+	/**
+	 * If direction set to `column` , it aligns items.
+	 */
+	alignItems: PropTypes.oneOf(['flex-start', 'center', 'flex-end', 'stretch', 'baseline']),
+	/**
+	 * If direction set to `row` , it justifies items.
+	 */
 	justify: PropTypes.oneOf([
 		'flex-start',
 		'center',
@@ -79,116 +91,59 @@ Grid.propTypes = {
 		'space-around',
 		'space-evenly',
 	]),
-	direction: PropTypes.oneOf([
-		'row',
-		'row-reverse',
-		'column',
-		'column-reverse',
-	]),
+	/**
+	 * Sets flex direction
+	 */
+	direction: PropTypes.oneOf(['row', 'row-reverse', 'column', 'column-reverse']),
+	/**
+	 * Sets flex wrapping
+	 */
 	wrap: PropTypes.oneOf(['nowrap', 'wrap', 'wrap-reverse']),
-	lg: PropTypes.oneOf([
-		false,
-		'auto',
-		true,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-	]),
-	md: PropTypes.oneOf([
-		false,
-		'auto',
-		true,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-	]),
-	sm: PropTypes.oneOf([
-		false,
-		'auto',
-		true,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-	]),
-	xl: PropTypes.oneOf([
-		false,
-		'auto',
-		true,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-	]),
-	xs: PropTypes.oneOf([
-		false,
-		'auto',
-		true,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-	]),
+	/**
+	 * The width of the column for screenclass `xs`, between 0 and 12
+	 */
+	xs: PropTypes.oneOf([false, true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+	/**
+	 * The width of the column for screenclass `sm`, between 0 and 12
+	 */
+	sm: PropTypes.oneOf([false, true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+	/**
+	 * The width of the column for screenclass `md`, between 0 and 12
+	 */
+	md: PropTypes.oneOf([false, true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+	/**
+	 * The width of the column for screenclass `lg`, between 0 and 12
+	 */
+	lg: PropTypes.oneOf([false, true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+	/**
+	 * The width of the column for screenclass `xl`, between 0 and 12
+	 */
+	xl: PropTypes.oneOf([false, true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+	/**
+	 * Sets gutter between child items when used as component type container,
+	 */
 	spacing: PropTypes.oneOf(GUTTERS),
+	/**
+	 * If `true`, it sets `min-width: 0` on the item.
+	 */
 	zeroMinWidth: PropTypes.bool,
 }
 
 Grid.defaultProps = {
-	component: 'div',
-	container: false,
 	alignContent: 'stretch',
 	alignItems: 'stretch',
-	justify: 'flex-start',
+	component: 'div',
+	container: false,
 	direction: 'row',
-	wrap: 'wrap',
-	xs: false,
-	sm: false,
-	md: false,
+	item: false,
+	justify: 'flex-start',
 	lg: false,
-	xl: false,
+	md: false,
+	sm: false,
 	spacing: 0,
+	wrap: 'wrap',
+	xl: false,
+	xs: false,
 	zeroMinWidth: false,
 }
 export default Grid
--- src/components/Grid/stories.js
+++ src/components/Grid/stories.js
@@ -1,71 +1,18 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { select } from '@storybook/addon-knobs'
 import Grid from './Grid'
 
 const stories = storiesOf('1. Components/Grid', module)
 
-export const justifyOptions = [
-	'flex-start',
-	'center',
-	'flex-end',
-	'space-between',
-	'space-around',
-	'space-evenly',
-]
-
-export const alignItemsOptions = [
-	'flex-start',
-	'center',
-	'flex-end',
-	'stretch',
-	'baseline',
-]
-
-export const alignContentOptions = [
-	'stretch',
-	'center',
-	'flex-start',
-	'flex-end',
-	'space-between',
-	'space-around',
-]
-export const directionOptions = [
-	'row',
-	'row-reverse',
-	'column',
-	'column-reverse',
-]
-
-export const wrapOptions = ['nowrap', 'wrap', 'wrap-reverse']
-
-export const breakpointOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-export const spacingOptions = [0, 8, 16, 24, 32, 40]
-
-export const createContainerKnobs = () => ({
-	justify: select('justify', justifyOptions, 'flex-start'),
-	alignItems: select('alignItems', alignItemsOptions, 'stretch'),
-	alignContent: select('alignContent', alignContentOptions, 'stretch'),
-	direction: select('direction', directionOptions, 'row'),
-	wrap: select('wrap', wrapOptions, 'wrap'),
-	spacing: select('spacing', spacingOptions, 0),
-})
-
-export const createItemKnobs = () => ({
-	xs: select('xs', breakpointOptions, 'auto'),
-	sm: select('sm', breakpointOptions, 'auto'),
-	md: select('md', breakpointOptions, 'auto'),
-	lg: select('lg', breakpointOptions, 'auto'),
-})
-
 export const Default = () => (
-	<Grid container {...createContainerKnobs()} style={{ height: '100vh' }}>
-		{Array.from(new Array(3), (val, index) => (
-			<Grid item {...createItemKnobs()} key={index}>
+	<Grid container spacing={8}>
+		{Array.from(new Array(12), (val, index) => (
+			<Grid item xs={12} sm={2} md={3} lg={4} xl={3} key={index}>
 				<div
 					style={{
 						padding: 16,
 						border: '1px solid rgba(0, 0, 0, 0.2)',
+						backgroundColor: '#eaeaf2',
 					}}
 				>
 					{`Cell ${index + 1}`}
