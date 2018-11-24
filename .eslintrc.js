0e368e23b454aebf4fc7cb36f4ecdff4b3a27e09
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-11-24 21:47:11 +0100

    :wrench: Update Eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
-{
-	"extends": ["react-app", "prettier"],
-	"rules": {
+module.exports = {
+	extends: ["react-app", "prettier"],
+	rules: {
 		"prettier/prettier": "error",
 		"arrow-body-style": "off",
 		"func-style": "off",
@@ -8,7 +8,9 @@
 		"no-invalid-this": "off",
 		"prefer-destructuring": "off",
 		"react/require-default-props": "off",
-		"indent": ["error", "tab"]
+		"indent": ["error", "tab"],
+		"linebreak-style": "off", // Don't play nicely with Windows
+		'react/jsx-wrap-multilines': 'off',
 	},
 	"plugins": ["prettier"]
 }
