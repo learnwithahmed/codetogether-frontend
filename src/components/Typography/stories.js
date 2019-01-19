e387b4424e04aa4d50ecaa0c4ffdefbc11bf2f55
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-01-19 22:09:26 +0100

    :wrench: Fix stylelint

--- src/components/Typography/stories.js
+++ src/components/Typography/stories.js
@@ -20,34 +20,32 @@ export const Default = () => (
 		<Typography variant="h4">Bold 24px</Typography>
 		<Typography variant="h5">Bold 20px</Typography>
 		<Typography variant="h6">Bold 16px</Typography>
-		<Typography variant="cite">
-			CITE 12PX. Lorem ipsum dolor sit amet
-		</Typography>
+		<Typography variant="cite">CITE 12PX. Lorem ipsum dolor sit amet</Typography>
 		<Typography variant="subtitle1">
-			BOLD 12PX. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-			Quos blanditiis tenetur
+			BOLD 12PX. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+			tenetur
 		</Typography>
 		<Typography variant="subtitle2">
-			REGULAR 11PX. subtitle1. Lorem ipsum dolor sit amet, consectetur
-			adipisicing elit. Quos blanditiis tenetur
+			REGULAR 11PX. subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+			blanditiis tenetur
 		</Typography>
 		<Typography variant="body1">
-			Body1 - Regular 16px . Lorem ipsum dolor sit amet, consectetur
-			adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
-			rerum inventore consectetur, neque doloribus, cupiditate numquam
-			dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+			Body1 - Regular 16px . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+			blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
+			doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+			quibusdam.
 		</Typography>
 		<Typography variant="body2">
-			Body2 - Bold 16px . Lorem ipsum dolor sit amet, consectetur
-			adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
-			rerum inventore consectetur, neque doloribus, cupiditate numquam
-			dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+			Body2 - Bold 16px . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+			blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
+			doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+			quibusdam.
 		</Typography>
 		<Typography variant="body3">
-			Body3 - Regular 14px . Lorem ipsum dolor sit amet, consectetur
-			adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
-			rerum inventore consectetur, neque doloribus, cupiditate numquam
-			dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+			Body3 - Regular 14px . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+			blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque
+			doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+			quibusdam.
 		</Typography>
 	</div>
 )
