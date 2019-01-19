2bfec86e3131f36d891f3d564eb59a15c8856b0f
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-01-19 22:13:28 +0100

    :zap: Add Courses and Single course pages

---
+++ src/pages/Course/stories.js
@@ -0,0 +1,232 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
+import AppBaseStory from '../../containers/AppBase/stories'
+import Container from '../../components/Container'
+import Media from '../../components/Media'
+import Grid from '../../components/Grid'
+import PageHeader from '../../components/PageHeader'
+import Section from '../../components/Section'
+import Typography from '../../components/Typography'
+import SeriesItem from '../../components/SeriesItem'
+import CourseSidebar from '../../components/CourseSidebar'
+import CourseSidebarInfo from '../../components/CourseSidebarInfo/CourseSidebarInfo'
+import styles from './styles.module.scss'
+
+const stories = storiesOf('3. Pages/Course', module)
+
+export const Default = () => (
+	<AppBaseStory>
+		<Section first>
+			<Container>
+				<Grid container spacing={32}>
+					<Grid item xs={12} md={8} lg={8} xl={8}>
+						<PageHeader>
+							<Typography variant="h2">Intro to Immutable JS</Typography>
+							<Typography
+								variant="body3"
+								style={{
+									lineHeight: 1.6,
+									fontSize: 17,
+									color: 'inherit',
+								}}
+							>
+								Learn about functional data structures and their uses in this
+								overview of Facebook's popular library for JavaScript: Immutable.js
+							</Typography>
+							<Typography
+								variant="h2"
+								style={{
+									fontSize: 22,
+								}}
+							>
+								Requirements
+							</Typography>
+							<ul className={styles.seriesRequirmentList}>
+								<li>
+									Make sure you have <code>node</code> instsalled on your machine
+								</li>
+								<li>Basics of JavaScript Data Structure</li>
+							</ul>
+							<Typography
+								variant="h2"
+								style={{
+									fontSize: 22,
+								}}
+							>
+								In this lesson, you will learn
+							</Typography>
+							<ul className={styles.seriesRequirmentList}>
+								<li>
+									throw a <code>Promise</code>
+								</li>
+								<li>
+									catch that <code>Promise with Suspense</code>
+								</li>
+								<li>add that data to cache</li>
+							</ul>
+						</PageHeader>
+					</Grid>
+				</Grid>
+				<Grid container spacing={32}>
+					<Grid item xs={12} md={8} lg={8} xl={8}>
+						<ul className={styles.seriesList}>
+							{Array.from(new Array(6), (val, index) => (
+								<SeriesItem key={index}>
+									<a href="#" className={styles.seriesCard}>
+										<div className={styles.seriesMediaWrapper}>
+											<Media
+												className={styles.seriesMedia}
+												component="img"
+												src="//placekitten.com/g/300/300"
+												alt={text('alt', 'Building grid layout')}
+											/>
+										</div>
+										<div className={styles.seriesContent}>
+											<small>
+												<strong>Episode</strong> 01 •{' '}
+												<strong>Duration</strong> 34:19
+											</small>
+											<Typography variant="h3" className={styles.seriesTitle}>
+												{text(
+													'title',
+													'How Subsets of an Immutable.js Map() works',
+												)}
+											</Typography>
+											<Typography
+												variant="body1"
+												className={styles.itemDescription}
+											>
+												{text(
+													'description',
+													'In this mini-series Im going to try and create website using Gatsby.js and WordPress.',
+												)}
+											</Typography>
+										</div>
+									</a>
+								</SeriesItem>
+							))}
+						</ul>
+					</Grid>
+					<Grid
+						item
+						xs={12}
+						md={4}
+						lg={4}
+						xl={4}
+						style={{
+							alignItems: 'flex-start',
+						}}
+					>
+						<CourseSidebar className={styles.sidebarSticky}>
+							<Media
+								className={styles.seriesMedia}
+								style={{
+									marginBottom: 0,
+									borderRadius: '4px 4px 0px 0px',
+									transitionTimingFunction: 'ease-out',
+								}}
+								component="img"
+								src="//placekitten.com/g/300/300"
+								alt={text('alt', 'Building grid layout')}
+							/>
+							<CourseSidebarInfo>
+								<img
+									src="/static/images/video.png"
+									width="24"
+									alt=""
+									className="course-icon"
+									style={{
+										marginTop: -1,
+										marginRight: 10,
+										opacity: 0.5,
+									}}
+								/>
+								<Typography
+									variant="h4"
+									style={{
+										display: 'inline-block',
+										fontSize: 17,
+										margin: 0,
+									}}
+								>
+									3
+								</Typography>
+								<Typography
+									variant="h4"
+									style={{
+										display: 'inline-block',
+										fontSize: 17,
+										margin: 0,
+									}}
+								>
+									&nbsp;video lessons
+								</Typography>
+							</CourseSidebarInfo>
+							<CourseSidebarInfo>
+								<img
+									src="/static/images/length.png"
+									width="24"
+									alt=""
+									className="course-icon"
+									style={{
+										marginTop: -1,
+										marginRight: 10,
+										opacity: 0.5,
+									}}
+								/>
+								<Typography
+									variant="h4"
+									style={{
+										display: 'inline-block',
+										fontSize: 17,
+										margin: 0,
+									}}
+								>
+									Intermediate
+								</Typography>
+							</CourseSidebarInfo>
+							<CourseSidebarInfo>
+								<img
+									src="/static/images/level.png"
+									width="24"
+									alt=""
+									className="course-icon"
+									style={{
+										marginTop: -1,
+										marginRight: 10,
+										opacity: 0.5,
+									}}
+								/>
+								<Typography
+									variant="h4"
+									style={{
+										display: 'inline-block',
+										fontSize: 17,
+										margin: 0,
+									}}
+								>
+									9
+								</Typography>
+								<Typography
+									variant="h4"
+									style={{
+										display: 'inline-block',
+										fontSize: 17,
+										margin: 0,
+									}}
+								>
+									&nbsp;min
+								</Typography>
+							</CourseSidebarInfo>
+						</CourseSidebar>
+					</Grid>
+				</Grid>
+			</Container>
+		</Section>
+	</AppBaseStory>
+)
+
+stories.add('Default', Default)
+
+export default Default
---
+++ src/pages/VideoCourses/stories.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text, boolean } from '@storybook/addon-knobs'
+import AppBaseStory from '../../containers/AppBase/stories'
+import Container from '../../components/Container'
+import Media from '../../components/Media'
+import Grid from '../../components/Grid'
+import PageHeader from '../../components/PageHeader'
+import Section from '../../components/Section'
+import Typography from '../../components/Typography'
+import Card from '../../components/Card'
+import PlayIcon from '../../components/icons/PlayIcon'
+import ArrowRight from '../../components/icons/ArrowRight'
+import styles from './styles.module.scss'
+const stories = storiesOf('3. Pages/VideoCourses', module)
+
+export const Default = () => (
+	<AppBaseStory>
+		<Section first>
+			<Container>
+				<Grid container>
+					<Grid xs={12}>
+						<PageHeader>
+							<Typography variant="h2">Video Courses</Typography>
+							<Typography variant="h5">
+								Learn web design with CodeTogether, from JS, React, Docker and
+								Microservices basics to the .
+							</Typography>
+						</PageHeader>
+					</Grid>
+				</Grid>
+				<Grid container spacing={24}>
+					{Array.from(new Array(6), (val, index) => (
+						<Grid item xs={12} sm={6} md={4} lg={4} key={index}>
+							<Card>
+								<a href="#" className={styles.courseLink}>
+									<div className={styles.relative}>
+										<Media
+											component="img"
+											src="//placekitten.com/g/300/300"
+											alt={text('alt', 'Building grid layout')}
+										/>
+										<PlayIcon className={styles.playIcon} />
+									</div>
+
+									<Typography
+										className={styles.courseTitle}
+										variant="h3"
+										color="inherit"
+									>
+										{text('title', 'Introduction to Immutable JS')}
+									</Typography>
+									<Typography
+										className={styles.courseCount}
+										variant="body2"
+										color="inherit"
+									>
+										{text('total', '3 VIDEOS')}
+									</Typography>
+									<Typography variant="body1" color="inherit">
+										{text(
+											'description',
+											'Ad consectetur duis excepteur pariatur Lorem eu reprehenderit ex labore ea incididunt adipisicing magna. Consectetur do et dolore consectetur sit incididunt nostrud quis commodo ut culpa ullamco Lorem voluptate. Cupidatat nostrud ea nostrud non adipisicing tempor Lorem. Minim cillum officia esse Lorem.',
+										)}
+									</Typography>
+									<div className={styles.viewCourseWrapper}>
+										<Typography variant="subtitle1" color="inherit">
+											{text('Link', 'View Course')}
+										</Typography>
+										<ArrowRight className={styles.arrowRight} />
+									</div>
+								</a>
+							</Card>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</Section>
+	</AppBaseStory>
+)
+
+stories.add('Default', Default)
+
+export default Default
